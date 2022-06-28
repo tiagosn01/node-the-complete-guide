4df8cc584246355e5d15b9a5872a90f1db20a85c
Tiago Silva <tiagosilva0922@gmail.com>
2022-06-28 14:55:51 -0300

    feat: pug templates

--- lessons/express/routes/shop.js
+++ lessons/express/routes/shop.js
@@ -1,14 +1,12 @@
-const path = require('path');
 
 const express = require('express');
 
-const rootDir = require('../utils/path');
 const adminData = require('./admin');
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.render('shop', {prods: adminData.products, docTitle: 'Shop'})
+  res.render('shop', {prods: adminData.products, pageTitle: 'Shop', path: '/'})
 });
 
 module.exports = router;
