98183812cb6bdc19915260216eab8823d6552da0
Tiago Silva <tiagosilva0922@gmail.com>
2022-06-28 23:07:38 -0300

    refacotr: using ejs

--- lessons/express/routes/admin.js
+++ lessons/express/routes/admin.js
@@ -1,15 +1,18 @@
+const path = require('path');
 
 const express = require('express');
 
-const productsControler = require('../controllers/products');
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
+// /admin/add-product => GET
+router.get('/add-product', adminController.getAddProduct);
 
+// /admin/products => GET
+router.get('/products', adminController.getProducts);
 
-router.get('/add-product', productsControler.getAddProduct)
-
-router.post('/add-product', productsControler.postAddProduct)
-
+// /admin/add-product => POST
+router.post('/add-product', adminController.postAddProduct);
 
 module.exports = router;
--- lessons/express/routes/shop.js
+++ lessons/express/routes/shop.js
@@ -1,10 +1,21 @@
+const path = require('path');
 
 const express = require('express');
 
-const productsControler = require('../controllers/products');
+const shopController = require('../controllers/shop');
 
 const router = express.Router();
 
-router.get('/', productsControler.getProducts);
+router.get('/', shopController.getIndex);
+
+router.get('/products', shopController.getProducts);
+
+router.get('/products/:productId', shopController.getProduct);
+
+router.get('/cart', shopController.getCart);
+
+router.get('/orders', shopController.getOrders);
+
+router.get('/checkout', shopController.getCheckout);
 
 module.exports = router;
